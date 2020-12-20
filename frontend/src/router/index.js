3d6f038fa42bcf5e3f3b11d4c44431a29ae0511c
taso0096 <jin2931121@yahoo.co.jp>
2020-12-21 08:57:17 +0900

    [Add]リダイレクト処理(Vue)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store';
+
 import Visualize from '@/views/Visualize.vue';
 
 Vue.use(VueRouter);
@@ -8,7 +10,10 @@ const routes = [
   {
     path: '/visualize',
     name: 'Visualize',
-    component: Visualize
+    component: Visualize,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '*',
@@ -20,6 +25,24 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const userData = store.state.userData;
+  if (to.matched.some(record => !record.meta.requiresAuth) || ((to.matched.some(record => !record.meta.requiresSuperuser) || userData.isSuperuser) && userData.isAuthed)) {
+    if ((to.name === 'Login' && userData.isAuthed)) {
+      next({ name: 'Visualize' });
+    } else {
+      next();
+    }
+  } else if (userData.isAuthed) {
+    next({ name: 'Visualize' });
+  } else {
+    next({
+      name: 'Login',
+      query: { redirect: to.fullPath }
+    });
+  }
 });
 
 export default router;
