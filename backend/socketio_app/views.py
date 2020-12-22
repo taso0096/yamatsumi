95ba7b4c213d93c95aaed4542193a938f3d2d13e
taso0096 <jin2931121@yahoo.co.jp>
2020-12-22 22:32:37 +0900

    [Fix]socketio設定(Django)

--- backend/socketio_app/views.py
+++ backend/socketio_app/views.py
@@ -1,13 +1,11 @@
 import socketio
 
-sio = socketio.AsyncServer(cors_allowed_origins='*')
+sio = socketio.Server(cors_allowed_origins='*')
 
 
-@sio.event
 async def connect(sid, environ):
     sio.emit('connected', room=sid)
 
 
-@sio.event
 async def disconnect(sid):
     print('disconnect:', sid)
