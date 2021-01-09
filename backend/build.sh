772d57fbe90b14c2f1279466ce01b510f5d1f477
taso0096 <jin2931121@yahoo.co.jp>
2021-01-10 02:46:09 +0900

    [Remove]管理ユーザの自動作成を削除(Django)

--- backend/build.sh
+++ backend/build.sh
@@ -3,4 +3,3 @@ rm -rf */migrations
 python manage.py makemigrations users
 python manage.py makemigrations networks
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'admin')" | python manage.py shell
