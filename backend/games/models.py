0a0d54f53a2431e292b6c33c224a3a7440e32aba
taso0096 <jin2931121@yahoo.co.jp>
2021-02-03 23:33:40 +0900

    [Add][Django][games] モデル作成

--- backend/games/models.py
+++ backend/games/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from networks.models import Network
 
-# Create your models here.
+
+class Game(models.Model):
+    network = models.ForeignKey(Network, on_delete=models.CASCADE)
+    version = models.CharField(max_length=10, blank=True, null=True)
+    teams = models.JSONField(default=dict, blank=True, null=True)
+    users = models.JSONField(default=dict, blank=True, null=True)
+    levels = models.JSONField(default=dict, blank=True, null=True)
+    categories = models.JSONField(default=dict, blank=True, null=True)
+    questions = models.JSONField(default=dict, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.network.id or self.id
