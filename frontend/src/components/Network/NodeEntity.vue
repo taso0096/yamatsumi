50b6962b4369d2cb346be271c0ef039456c3e80b
taso0096 <jin2931121@yahoo.co.jp>
2021-02-12 01:56:16 +0900

    [Add][Vue][QuestionEntity] 問題表示コンポーネント作成

--- frontend/src/components/Network/NodeEntity.vue
+++ frontend/src/components/Network/NodeEntity.vue
@@ -1,5 +1,10 @@
 <template>
-  <a-entity v-if="!validNode.label"></a-entity>
+  <QuestionEntity
+    v-if="validNode.questions"
+    :node="node"
+    :questions="validNode.questions"
+  />
+  <a-entity v-else-if="!validNode.label"></a-entity>
   <a-entity v-else>
     <a-sphere
       :radius="sphereRadius*validNode.size"
@@ -18,8 +23,13 @@
 </template>
 
 <script>
+import QuestionEntity from './QuestionEntity.vue';
+
 export default {
   name: 'NodeEntity',
+  components: {
+    QuestionEntity
+  },
   props: {
     node: {
       type: Object
@@ -32,6 +42,24 @@ export default {
     sphereRadius: () => 0.2
   },
   mounted() {
+    if (this.node.nodeOptions?.type === 'question') {
+      const questions = JSON.parse(JSON.stringify(this.$_event.game.questions));
+      const tags = this.node.id.split('__');
+      tags.forEach(tag => {
+        const levelId = tag.split(/^level-/)[1];
+        const categoryId = tag.split(/^category-/)[1];
+        const tmpQuestions = JSON.parse(JSON.stringify(questions));
+        if (levelId) {
+          questions.length = 0;
+          questions.push(...tmpQuestions.filter(q => q.levelId === levelId));
+        } else if (categoryId) {
+          questions.length = 0;
+          questions.push(...tmpQuestions.filter(q => q.categoryId === categoryId));
+        }
+      });
+      this.$set(this.validNode, 'questions', questions);
+      return;
+    };
     this.$set(this.validNode, 'label', this.node.label || this.node.id);
     this.$set(this.validNode, 'type', this.node.nodeOptions?.type || 'sphere');
     this.$set(this.validNode, 'size', this.node.nodeOptions?.size || 1);
---
+++ frontend/src/components/Network/QuestionEntity.vue
@@ -0,0 +1,77 @@
+<template>
+  <a-entity
+    :id="`question-entity__${node.id}`"
+    :scale="`${node.nodeOptions.size} ${node.nodeOptions.size} ${node.nodeOptions.size}`"
+  >
+    <a-text
+      :value="node.label"
+      align="center"
+      :color="node.nodeOptions.labelColor"
+      side="double"
+      :position="`0 ${-sphereRadius*node.nodeOptions.size - sphereRadius} 0`"
+      wrap-count="50"
+    />
+    <a-circle
+      v-for="(question, i) in questions"
+      :key="question.id"
+      :id="`question__${question.id}`"
+      :color="node.nodeOptions.nodeColor"
+      side="double"
+      radius="0.2"
+      :position="question.position"
+      :look-center="`parentSelector: question-entity__${node.id}`"
+    >
+      <a-ring
+        :color="node.nodeOptions.nodeColor"
+        side="double"
+        radius-inner="0.205"
+        radius-outer="0.21"
+      />
+      <a-ring
+        :color="node.nodeOptions.nodeColor"
+        side="double"
+        radius-inner="0.215"
+        radius-outer="0.235"
+      />
+      <a-text
+        :value="i + 1"
+        align="center"
+        :color="node.nodeOptions.labelColor"
+        side="double"
+        position="0 0.01 0"
+        rotation="0 180 0"
+      />
+    </a-circle>
+  </a-entity>
+</template>
+
+<script>
+export default {
+  name: 'QuestionEntity',
+  props: {
+    node: {
+      type: Object,
+      required: true
+    },
+    questions: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    sphereRadius: () => 0.45
+  },
+  mounted() {
+    const questionCount = this.questions.length;
+    const questionRadius = 0.65;
+    for (const i of [...Array(questionCount).keys()]) {
+      const theta = Math.acos(-1 + 2*(i + 1)/(questionCount + 1)); // 仰角
+      const phi = Math.sqrt((questionCount + 1)*Math.PI)*theta; // 俯角
+      const x = questionRadius*Math.sin(theta)*Math.cos(phi);
+      const y = questionRadius*Math.sin(theta)*Math.sin(phi);
+      const z = questionRadius*Math.cos(theta);
+      this.$set(this.questions[i], 'position', new THREE.Vector3(x, y, z));
+    }
+  }
+};
+</script>
