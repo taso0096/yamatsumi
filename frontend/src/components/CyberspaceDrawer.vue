12a19e5dee01290b3509efccfd9334b4b846cf38
taso0096 <jin2931121@yahoo.co.jp>
2021-09-26 14:34:55 +0900

    [Add] DetailsEditorコンポーネント作成

--- frontend/src/components/CyberspaceDrawer.vue
+++ frontend/src/components/CyberspaceDrawer.vue
@@ -10,45 +10,49 @@
     width="50%"
     class="cyberspace-drawer pt-3 pr-3"
   >
-    <v-toolbar flat>
-      <v-tabs v-model="drawerTab">
-        <v-tab>Details</v-tab>
-        <v-tab disabled>Contest</v-tab>
-        <v-tab>Network</v-tab>
-        <v-spacer />
-        <v-menu
-          bottom
-          left
-          offset-y
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              icon
-              v-bind="attrs"
-              v-on="on"
-              class="my-auto mr-3"
-            >
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </template>
+    <v-sheet>
+      <v-toolbar flat color="transparent">
+        <v-tabs v-model="drawerTab">
+          <v-tab>Details</v-tab>
+          <v-tab disabled>Contest</v-tab>
+          <v-tab>Network</v-tab>
+          <v-spacer />
+          <v-menu
+            bottom
+            left
+            offset-y
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                v-bind="attrs"
+                v-on="on"
+                class="my-auto mr-3"
+              >
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
 
-          <v-list class="pa-0">
-            <v-list-item>
-              <v-list-item-title>Edit</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title color="error--text">Delete</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-tabs>
-    </v-toolbar>
+            <v-list class="pa-0">
+              <v-list-item>
+                <v-list-item-title>Edit</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title color="error--text">Delete</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-tabs>
+      </v-toolbar>
+    </v-sheet>
 
     <v-tabs-items
       v-model="drawerTab"
-      class="cyberspace-drawer__tab-items"
+      class="cyberspace-drawer__tab-items mt-3"
     >
-      <v-tab-item>Details</v-tab-item>
+      <v-tab-item>
+        <details-editor :cyberspace="cyberspace" />
+      </v-tab-item>
       <v-tab-item>Contest</v-tab-item>
       <v-tab-item>
         <network-editor
@@ -69,11 +73,13 @@
 </style>
 
 <script>
+import DetailsEditor from '@/components/DetailsEditor.vue';
 import NetworkEditor from '@/components/NetworkEditor';
 
 export default {
   name: 'CyberspaceDrawer',
   components: {
+    DetailsEditor,
     NetworkEditor
   },
   props: {
