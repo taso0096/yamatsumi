712750f4272cbe29549c7f301072cf51087ab0eb
taso0096 <jin2931121@yahoo.co.jp>
2021-11-06 23:36:48 +0900

    [Update] 演習テスト用API実装

--- backend/example/urls.py
+++ backend/example/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from example import views
 
 urlpatterns = [
+    path('scores/', views.ScoresView.as_view(), name='score example'),
+    path('questions/', views.QuestionsView.as_view(), name='questions example'),
 ]
--- backend/example/views.py
+++ backend/example/views.py
@@ -1,3 +1,131 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class ScoresView(GenericAPIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, _):
+        score_data = {
+            'team_a': {
+                'user_1': 0,
+                'user_2': 100,
+                'user_3': 500,
+                'user_4': 1000
+            },
+            'team_b': {
+                'user_5': 0,
+                'user_6': 0,
+                'user_7': 0,
+                'user_8': 0
+            },
+            'team_c': {
+                'user_9': 0,
+                'user_10': 0,
+                'user_11': 0,
+                'user_12': 0
+            },
+            'team_d': {
+                'user_13': 0,
+                'user_14': 0,
+                'user_15': 0,
+                'user_16': 0
+            },
+            'team_e': {
+                'user_17': 0,
+                'user_18': 0,
+                'user_19': 0,
+                'user_20': 0
+            }
+        }
+        return Response(data=score_data, status=status.HTTP_200_OK)
+
+
+class QuestionsView(GenericAPIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, _):
+        questions_data = {
+            'levels': [
+                {
+                    'id': 'easy',
+                    'label': 'Easy',
+                    'score': 100
+                },
+                {
+                    'id': 'normal',
+                    'label': 'Normal',
+                    'score': 200
+                },
+                {
+                    'id': 'hard',
+                    'label': 'Hard',
+                    'score': 300
+                }
+            ],
+            'questions': [
+                {
+                    'id': 1,
+                    'levelId': 'easy'
+                },
+                {
+                    'id': 2,
+                    'levelId': 'easy'
+                },
+                {
+                    'id': 3,
+                    'levelId': 'easy'
+                },
+                {
+                    'id': 4,
+                    'levelId': 'easy'
+                },
+                {
+                    'id': 5,
+                    'levelId': 'easy'
+                },
+                {
+                    'id': 6,
+                    'levelId': 'normal'
+                },
+                {
+                    'id': 7,
+                    'levelId': 'normal'
+                },
+                {
+                    'id': 8,
+                    'levelId': 'normal'
+                },
+                {
+                    'id': 9,
+                    'levelId': 'normal'
+                },
+                {
+                    'id': 10,
+                    'levelId': 'normal'
+                },
+                {
+                    'id': 11,
+                    'levelId': 'hard'
+                },
+                {
+                    'id': 12,
+                    'levelId': 'hard'
+                },
+                {
+                    'id': 13,
+                    'levelId': 'hard'
+                },
+                {
+                    'id': 14,
+                    'levelId': 'hard'
+                },
+                {
+                    'id': 15,
+                    'levelId': 'hard'
+                }
+            ]
+        }
+        return Response(data=questions_data, status=status.HTTP_200_OK)
