7e0351b706a01a32da6f16f1f81570203268ff72
taso0096 <jin2931121@yahoo.co.jp>
2021-11-12 20:29:05 +0900

    [Update] 演習テスト用API修正

--- backend/example/views.py
+++ backend/example/views.py
@@ -10,34 +10,49 @@ class ScoresView(GenericAPIView):
     def get(self, _):
         score_data = {
             'team_a': {
-                'user_1': 0,
-                'user_2': 100,
-                'user_3': 500,
-                'user_4': 1000
+                'score': 1600,
+                'users': {
+                    'user_1': 0,
+                    'user_2': 100,
+                    'user_3': 500,
+                    'user_4': 1000
+                }
             },
             'team_b': {
-                'user_5': 0,
-                'user_6': 0,
-                'user_7': 0,
-                'user_8': 0
+                'score': 0,
+                'users': {
+                    'user_5': 0,
+                    'user_6': 0,
+                    'user_7': 0,
+                    'user_8': 0
+                }
             },
             'team_c': {
-                'user_9': 0,
-                'user_10': 0,
-                'user_11': 0,
-                'user_12': 0
+                'score': 0,
+                'users': {
+                    'user_9': 0,
+                    'user_10': 0,
+                    'user_11': 0,
+                    'user_12': 0
+                }
             },
             'team_d': {
-                'user_13': 0,
-                'user_14': 0,
-                'user_15': 0,
-                'user_16': 0
+                'score': 0,
+                'users': {
+                    'user_13': 0,
+                    'user_14': 0,
+                    'user_15': 0,
+                    'user_16': 0
+                }
             },
             'team_e': {
-                'user_17': 0,
-                'user_18': 0,
-                'user_19': 0,
-                'user_20': 0
+                'score': 0,
+                'users': {
+                    'user_17': 0,
+                    'user_18': 0,
+                    'user_19': 0,
+                    'user_20': 0
+                }
             }
         }
         return Response(data=score_data, status=status.HTTP_200_OK)
